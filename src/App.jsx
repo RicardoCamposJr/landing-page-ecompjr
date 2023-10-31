f04c9b6e5e59c94dd3e648477d3c71b8ba58a3c4
RicardoCamposJr <rickoliver001@hotmail.com>
2023-10-31 12:52:04 -0300

    :lipstick: feat: Estilização da barra de navegação
    Estilização da barra de navegação da página: animação do ícone do instagram e centralização dos elementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //import { useState } from 'react'
+//import style from "../src/styles/App.module.css"
 import Header from "./components/Header"
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import style from "../styles/Header.module.css"
 import nameLogo from "../assets/name-logo.png"
-import fullLogo from "../assets/full-logo.png"
+// Import do componente do Font Awesome:
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+// Import do ícone de instagram do Font Awesome:
+import {faInstagram} from "@fortawesome/free-brands-svg-icons"
 
 export default function Header() {
     return (
         <div className={style.header}>
-            <div className={style.icon}>
-                <a href="#">insta</a>
-            </div>
-            <div className="header-logo">
+            {/* ícone do Font Awesome */}
+            <a href="https://www.instagram.com/ecompjr/">
+                <FontAwesomeIcon icon={faInstagram} className={style.icon} />
+            </a>
+            <div className={style.headerLogo}>
                 <img src= {nameLogo} alt="Logo da EcompJr" />
             </div>
             <div className="nav">
-                
+                <a href= {style.header}>Início</a>
+                <a href="#">Sobre</a>
+                <a href="#">Serviços</a>
+                <a href="#">Contato</a>
             </div>
         </div>
     )
