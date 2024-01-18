c8f5a824e283c4683a121100767bafe995d93c77
Ricardo Campos <rickoliver001@hotmail.com>
2024-01-18 20:43:43 -0300

    final

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
-import Navbar from "../components/header/Navbar"
+import Navbar from "../components/header/Navbar.jsx"
 import SignUp from "../pages/SignUp"
 import Messages from "../pages/Messages"
 
