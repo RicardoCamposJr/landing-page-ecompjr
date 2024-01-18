5aff6f6412b6a63773d6fd9d26b45962a5c38b7d
Ricardo Campos <rickoliver001@hotmail.com>
2024-01-18 20:56:16 -0300

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import About from "../components/About/About";
 import HeaderFullLogo from "../components/Header/HeaderFullLogo";
 import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
-import Navbar from "../components/header/Navbar";
+import Navbar from "../components/Header/Navbar";
 import MVV from "../components/mission-vision-value/MVV";
 import Services from "../components/services/Services";
 
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
-import Navbar from "../components/header/Navbar.jsx"
+import Navbar from "../components/Header/Navbar"
 import SignUp from "../pages/SignUp"
 import Messages from "../pages/Messages"
 
